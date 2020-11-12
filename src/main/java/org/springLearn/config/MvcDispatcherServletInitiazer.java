6a124bce77454e7366598e117daca69f1ce6ac59
Alexey Ermolenko <alexey.ermolenko@saritasa.com>
2020-11-12 17:16:29 +0500

    Add configuration in java files and create two new controllers

---
+++ src/main/java/org/springLearn/config/MvcDispatcherServletInitiazer.java
@@ -0,0 +1,20 @@
+package org.springLearn.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcDispatcherServletInitiazer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {Config.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
